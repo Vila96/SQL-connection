514470e2139eda9c43dbec2707a66c5208ac06bc
Tomas <tomas.vila.sanchez@gmail.com>
2021-01-01 21:56:08 +0100

    minor changes

--- front/react-node-sql/src/App.js
+++ front/react-node-sql/src/App.js
@@ -1,19 +1,23 @@
 import './App.css';
+import {useState} from "react";
 
 function App() {
+
+  let [testDb, setTestDb] = useState("");
+
   return (
     <div className="App">
         <h1>My CRUD app</h1>
-        <label>Search in DB</label>
+        <label>Search in DB:</label>
         <input type="text" name="searchTest"></input>
         <button>Search</button>
 
 
-        <div>
           <h3>Coming soon</h3>
-          <input type="text" name="addTest"></input>
+          <label>Add:</label>
+          <input type="text" name="addTest" onChange={(event) => {setTestDb = event.target.value}}></input>
+          <label>Remove:</label>
           <input type="text" name="removeTest"></input>
-        </div>
     </div>
 
   );
